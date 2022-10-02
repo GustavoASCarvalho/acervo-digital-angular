17a13acc452e7e2e49cd07e3e5cb698cf7ba59f7
Gustavo Alexandre <gustavoalexandrescarvalho@gmail.com>
2022-10-02 01:21:59 -0300

    feat(header): melhorando navegação com tab

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ThemeService } from 'src/app/core/theme.service';
+
+enum headerState {
+  'INICIAL' = 0,
+  'BUSCANDO' = 1,
+  'SLIDEBAR' = 2,
+}
 
 @Component({
   selector: 'app-header',
@@ -8,35 +14,12 @@ import { ThemeService } from 'src/app/core/theme.service';
 })
 export class HeaderComponent {
   usuarioLogado = false;
-  estaBuscando = false;
+  estado: headerState = headerState.INICIAL;
 
-  @Input() indexSelecionado: number = 0;
+  constructor(private themeService: ThemeService, private el: ElementRef) {}
 
-  constructor(private themeService: ThemeService) {}
-
-  mostrarBusca() {
-    document.querySelector('.header__menu')?.classList.add('hide');
-    document.querySelector('.header__search')?.classList.remove('hide');
-    document.querySelector('.overlay')?.classList.add('show');
-    document.querySelector('input')?.focus();
-  }
-
-  fecharBusca() {
-    document.querySelector('.header__menu')?.classList.remove('hide');
-    document.querySelector('.header__search')?.classList.add('hide');
-    document.querySelector('.overlay')?.classList.remove('show');
-  }
-
-  abrirMenu() {
-    document.querySelector('.header__menu')?.classList.add('hide');
-    document.querySelector('.slider')?.classList.remove('hide');
-    document.querySelector('.overlay')?.classList.add('show');
-  }
-
-  fecharMenu() {
-    document.querySelector('.header__menu')?.classList.remove('hide');
-    document.querySelector('.slider')?.classList.add('hide');
-    document.querySelector('.overlay')?.classList.remove('show');
+  alterarEstado(estado: headerState) {
+    this.estado = estado;
   }
 
   trocarTema() {
@@ -52,24 +35,38 @@ export class HeaderComponent {
   }
 
   ngAfterViewInit() {
-    window.addEventListener('scroll', (_) => {
-      let heigth = window.scrollY;
-      if (heigth > 0) {
+    //   //adiciona o efeito de sombra quando o usuario scrolla a pagina
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
         document.body.querySelector('.header')?.classList.add('shadow');
       } else {
         document.body.querySelector('.header')?.classList.remove('shadow');
       }
     });
-    window.addEventListener('click', (e) => {
-      if (e.target === document.querySelector('.overlay')) {
-        this.fecharBusca();
-        this.fecharMenu();
+    //fecha qualquer menu que esteja aberto quando o usuario clica fora dele
+    this.el.nativeElement.addEventListener(
+      'click',
+      (e: { target: Element }) => {
+        if (e.target === document.querySelector('.overlay')) {
+          this.alterarEstado(headerState.INICIAL);
+        }
       }
-    });
-    window.addEventListener('keyup', (e) => {
-      if (e.key === 'Escape' || e.key === 'Esc') {
-        this.fecharBusca();
-        this.fecharMenu();
+    );
+    //fecha qualquer menu que esteja aberto quando o usuario aperta esc no teclado ou foca em algum outro elemento da tela com tab
+    window.addEventListener('keyup', (e: { key: string }) => {
+      if (e.key == 'Escape' || e.key == 'Esc') {
+        this.alterarEstado(headerState.INICIAL);
+      }
+      if (e.key == 'Tab') {
+        //verificar se o elemento que esta com foco é um elemento do header
+        if (
+          document.activeElement?.closest('.header') !==
+            this.el.nativeElement.querySelector('.header') &&
+          document.activeElement?.closest('.slider') !==
+            this.el.nativeElement.querySelector('.slider')
+        ) {
+          this.alterarEstado(headerState.INICIAL);
+        }
       }
     });
   }
