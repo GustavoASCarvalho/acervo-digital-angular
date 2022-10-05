d0979a8e35414c4d794e25f66548953e57936e80
Gustavo Alexandre <gustavoalexandrescarvalho@gmail.com>
2022-10-05 20:09:17 -0300

    feat: novo component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, CardCarouselComponent],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, CardCarouselComponent],
 })
 export class SharedModule {}
