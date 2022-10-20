31e4d9feb09a5ccc64da7ca3070482fe60552c88
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-10-20 18:18:35 -0300

    feat: questão do dia

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeComponent } from './page/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
 import { GridPostagensComponent } from './components/grid-postagens/grid-postagens.component';
+import { QuestionSectionComponent } from './components/question-section/question-section.component';
 
 @NgModule({
-  declarations: [HomeComponent, CardCarouselComponent, GridPostagensComponent],
+  declarations: [HomeComponent, CardCarouselComponent, GridPostagensComponent, QuestionSectionComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
