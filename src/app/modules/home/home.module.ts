4a835f4c4e9aa1f2a5d9c2957ae18a3aec97e5a0
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-10-21 13:37:26 -0300

    feat: separando tags da home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -7,9 +7,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CardCarouselComponent } from './components/card-carousel/card-carousel.component';
 import { GridPostagensComponent } from './components/grid-postagens/grid-postagens.component';
 import { QuestionSectionComponent } from './components/question-section/question-section.component';
+import { TagsSectionComponent } from './components/tags-section/tags-section.component';
 
 @NgModule({
-  declarations: [HomeComponent, CardCarouselComponent, GridPostagensComponent, QuestionSectionComponent],
+  declarations: [HomeComponent, CardCarouselComponent, GridPostagensComponent, QuestionSectionComponent, TagsSectionComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
