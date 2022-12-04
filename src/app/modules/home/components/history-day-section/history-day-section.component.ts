5b3f826e5b0138809d3b80a7eb7ff9749e8eb19d
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2022-12-04 11:18:20 -0300

    feat: dia na história um pouco mais dinamico

--- src/app/modules/home/components/history-day-section/history-day-section.component.ts
+++ src/app/modules/home/components/history-day-section/history-day-section.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryDaySectionComponent implements OnInit {
 
+  historyDay = {
+    title: 'Neste dia em 1821 acontecia a Indepência do Brasil',
+    description: 'O Dia da Independência do Brasil é comemorado em 7 de setembro. A data marca a independência política do Brasil em relação à Coroa Portuguesa, que se deu em 1822, após a proclamação da República, em 15 de novembro de 1889, e a independência econômica em 1945, com a criação do Banco Central do Brasil.',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
