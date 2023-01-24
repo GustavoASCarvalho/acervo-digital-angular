68e92832d352b184f4316294acc6d00000923ac8
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2023-01-24 17:29:25 -0300

    feat: inicio tela acervo

--- src/app/modules/acervo/page/acervo/acervo.component.ts
+++ src/app/modules/acervo/page/acervo/acervo.component.ts
@@ -5,4 +5,103 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './acervo.component.html',
   styleUrls: ['./acervo.component.scss'],
 })
-export class AcervoComponent {}
+export class AcervoComponent {
+  itens: any[] = [
+    {
+      title: 'Imagem 1',
+      imageUrl: 'https://picsum.photos/800/1200?random=1',
+    },
+    {
+      title: 'Imagem 2',
+      imageUrl: 'https://picsum.photos/800/1200?random=2',
+    },
+    {
+      title: 'Imagem 3',
+      imageUrl: 'https://picsum.photos/800/1200?random=3',
+    },
+    {
+      title: 'Imagem 4',
+      imageUrl: 'https://picsum.photos/800/1200?random=4',
+    },
+    {
+      title: 'Imagem 5',
+      imageUrl: 'https://picsum.photos/800/1200?random=5',
+    }, {
+      title: 'Imagem 1',
+      imageUrl: 'https://picsum.photos/800/1200?random=1',
+    },
+    {
+      title: 'Imagem 2',
+      imageUrl: 'https://picsum.photos/800/1200?random=2',
+    },
+    {
+      title: 'Imagem 3',
+      imageUrl: 'https://picsum.photos/800/1200?random=3',
+    },
+    {
+      title: 'Imagem 4',
+      imageUrl: 'https://picsum.photos/800/1200?random=4',
+    },
+    {
+      title: 'Imagem 5',
+      imageUrl: 'https://picsum.photos/800/1200?random=5',
+    }, {
+      title: 'Imagem 1',
+      imageUrl: 'https://picsum.photos/800/1200?random=1',
+    },
+    {
+      title: 'Imagem 2',
+      imageUrl: 'https://picsum.photos/800/1200?random=2',
+    },
+    {
+      title: 'Imagem 3',
+      imageUrl: 'https://picsum.photos/800/1200?random=3',
+    },
+    {
+      title: 'Imagem 4',
+      imageUrl: 'https://picsum.photos/800/1200?random=4',
+    },
+    {
+      title: 'Imagem 5',
+      imageUrl: 'https://picsum.photos/800/1200?random=5',
+    }, {
+      title: 'Imagem 1',
+      imageUrl: 'https://picsum.photos/800/1200?random=1',
+    },
+    {
+      title: 'Imagem 2',
+      imageUrl: 'https://picsum.photos/800/1200?random=2',
+    },
+    {
+      title: 'Imagem 3',
+      imageUrl: 'https://picsum.photos/800/1200?random=3',
+    },
+    {
+      title: 'Imagem 4',
+      imageUrl: 'https://picsum.photos/800/1200?random=4',
+    },
+    {
+      title: 'Imagem 5',
+      imageUrl: 'https://picsum.photos/800/1200?random=5',
+    }, {
+      title: 'Imagem 1',
+      imageUrl: 'https://picsum.photos/800/1200?random=1',
+    },
+    {
+      title: 'Imagem 2',
+      imageUrl: 'https://picsum.photos/800/1200?random=2',
+    },
+    {
+      title: 'Imagem 3',
+      imageUrl: 'https://picsum.photos/800/1200?random=3',
+    },
+    {
+      title: 'Imagem 4',
+      imageUrl: 'https://picsum.photos/800/1200?random=4',
+    },
+    {
+      title: 'Imagem 5',
+      imageUrl: 'https://picsum.photos/800/1200?random=5',
+    },
+  ];
+}
