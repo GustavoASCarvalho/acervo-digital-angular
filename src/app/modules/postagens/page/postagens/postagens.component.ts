0c13047a131a623188a9ee0398d22390a2b3f0f9
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2023-02-01 22:08:55 -0300

    feat: tela de histórias

--- src/app/modules/postagens/page/postagens/postagens.component.ts
+++ src/app/modules/postagens/page/postagens/postagens.component.ts
@@ -12,4 +12,103 @@ export class PostagensComponent implements OnInit {
   ngOnInit(): void {
     window.scrollTo(0, 0);
   }
+
+  itens: any[] = [
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=1',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=2',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=3',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=4',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=1',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=2',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=3',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=4',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=1',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=2',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=3',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=4',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=1',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=2',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=3',
+    },
+    {
+      data: '2020/10/10',
+      titulo: 'Praça do leão de Paranaguá',
+      descricaoCurta: 'A praça do leão é um dos pontos turísticos mais visitados de Paranaguá. A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.A praça do leão é um dos pontos turísticos mais visitados de Paranaguá.',
+      urlImagem: 'https://picsum.photos/400/400?random=4',
+    },
+  ]
 }
