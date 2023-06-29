5f7790584b032d691e8bbb659bed2e2d6cccb4eb
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2023-06-28 21:38:57 -0300

    chore: completar código de verificação "on paste"

--- src/app/modules/cadastro/page/cadastro.component.ts
+++ src/app/modules/cadastro/page/cadastro.component.ts
@@ -1,32 +1,35 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 enum Steps {
-InserirEmail = 1,
-ConfirmarEmail = 2,
-ConfirmarSenha = 3,
+  InserirEmail = 1,
+  ConfirmarEmail = 2,
+  ConfirmarSenha = 3,
 }
 
 @Component({
-selector: 'app-cadastro',
-templateUrl: './cadastro.component.html',
-styleUrls: ['./cadastro.component.scss']
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.scss'],
 })
 export class CadastroComponent implements OnInit {
-
   step = Steps.InserirEmail;
-
 
   @ViewChild('containerFormCodigo')
   containerFormCodigo!: ElementRef;
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   trocarStep(step: Steps) {
     this.step = step;
-    if(step == Steps.ConfirmarEmail){
+    if (step == Steps.ConfirmarEmail) {
       setTimeout(() => {
         this.manipularInputs();
       }, 100);
@@ -34,22 +37,46 @@ export class CadastroComponent implements OnInit {
   }
 
   manipularInputs() {
-    const container = this.el.nativeElement.querySelector(".container__form__codigo");
+    const container = this.el.nativeElement.querySelector(
+      '.container__form__codigo'
+    );
     if (container) {
-      const inputs = container.querySelectorAll("input");
-      inputs.forEach((input: { addEventListener: (arg0: string, arg1: (event: any) => void) => void; }, index: number) => {
-        input.addEventListener("input", (event) => {
-          if(event.inputType == "deleteContentBackward"){
-            if(index > 0){
-              inputs[index - 1].focus();
+      const inputs = container.querySelectorAll('input');
+      const next_button = this.el.nativeElement.querySelector(
+        '.container__form__content #next_button'
+      );
+      inputs.forEach(
+        (
+          input: {
+            addEventListener: (
+              arg0: string,
+              arg1: (event: any) => void
+            ) => void;
+          },
+          index: number
+        ) => {
+          input.addEventListener('input', (event) => {
+            if (event.inputType == 'deleteContentBackward') {
+              if (index > 0) {
+                inputs[index - 1].focus();
+              }
+            } else {
+              if (index < inputs.length - 1) {
+                inputs[index + 1].focus();
+              }
             }
-          }else {
-            if(index < inputs.length - 1){
-              inputs[index + 1].focus();
+          });
+          input.addEventListener('paste', (event) => {
+            event.preventDefault();
+            const data = event.clipboardData.getData('text');
+            for (let i = 0; i < 4; i++) {
+              inputs[i].value = data[i];
             }
-          }
-        });
-      });
+
+            next_button.click();
+          });
+        }
+      );
     }
   }
 }
